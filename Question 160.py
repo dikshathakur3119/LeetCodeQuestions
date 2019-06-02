a19a169e0ca97b1fb5722ec23b446a41dcfff29f
dikshathakur3119 <dikshathakur3119@gmail.com>
2019-06-02 19:38:00 -0400

    Intersection of Two Linked Lists

--- Question 160.py
+++ Question 160.py
@@ -7,4 +7,61 @@ Input Explanation: The intersected node's value is 8 (note that this must not be
 
 '''
 
-def 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        def getLen(givenList):
+            l = 0
+            while givenList is not None:
+                l += 1
+                givenList = givenList.next
+            return l
+        
+        
+        def makeLenEqual(list1, list2):
+            len1 = getLen(list1)
+            len2 = getLen(list2)
+            
+            if len1>len2:
+                longList, shortList = list1, list2
+                longLen, shortLen = len1, len2
+            else:
+                longList, shortList = list2, list1
+                longLen, shortLen = len2, len1
+            
+            
+            while longLen > shortLen:
+                longList = longList.next
+                longLen = longLen - 1
+
+            return longList, shortList
+        
+        
+        intersectNode = None
+        
+        listA , listB = makeLenEqual(headA, headB)
+        
+        while listA is not None and listB is not None:
+            if listA == listB:
+                if intersectNode == None:
+                    intersectNode = listA
+            else:
+                intersectNode = None
+            listA = listA.next
+            listB = listB.next
+        
+        
+        return intersectNode
+        
+            
+                
+                
